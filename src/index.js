7c128c9c9db4b6f5fb72351fa1ad948ba35d80d0
PMENOS.COM.BR\139490 <gabriellisboa@pmenos.com.br>
2025-03-12 20:47:27 -0300

    dependencies: adicionando dependencias do next para a implementação do front end. Também incluindo a melhor organização das pastas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import express from "express";
-//import pkg from "body-parser";
 import router from "./routes/router.js";
-import db from './database/database.js'
 
 const app = express();
-
-//const { json, urlEncoded } = pkg;
 
 app.use(express.json());
 app.use(express.urlencoded({extented: true}));
