0011b52e946033202ca91d1a1d5f3f4305f45ddb
PMENOS.COM.BR\139490 <gabriellisboa@pmenos.com.br>
2025-03-17 14:02:18 -0300

    implementações das outras páginas da aplicação

--- medi-app/src/app/home/page.tsx
+++ medi-app/src/app/home/page.tsx
@@ -9,8 +9,13 @@ export const Home = () => {
         <h1>Olá</h1>
         <div>
             <Link href={"/doctor/create"}>Create new doctor</Link>
+            <br />
+            <Link href={"/doctor/list"}>List all doctors</Link>
+            <br />
             <Link href={"/appointment/create"}>Create new appointment</Link>
+            <br />
             <Link href={"/pacient/create"}>Create new pacient</Link>
+            <br />
             <Link href={"/prescription/create"}>Create new prescription</Link>
         </div>
         </>
--- medi-app/src/app/page.tsx
+++ medi-app/src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
@@ -36,6 +37,25 @@ export default function Home() {
   }
 
   return (
+    <div>
+      <form className='w-full' onSubmit={authentication}>
+        <span className='font-bold text-blue-500 py-2 block text-2xl'>Login</span>
+        <div className='w-full py-2'>
+          <label htmlFor="" className='text-sm font-bold py-2 block'>Usuário</label>
+          <input type='text' name='name' className='w-full border-[1px] border-gray-200 p-2 rounded-sm' onChange={(e: any) => setLogin(e.target.value)} />
+        </div>
+        <div className='w-full py-2'>
+          <label htmlFor="" className='text-sm font-bold py-2 block'>Senha</label>
+          <input name='login' type="password" className='w-full border-[1px] border-gray-200 p-2 rounded-sm' onChange={(e: any) => setPassword(e.target.value)} />
+        </div>
+        <div className='w-full py-2'>
+          <button className="w-20 p-2 text-white border-gray-200 border-[1px] rounded-sm bg-green-400">Login</button>
+        </div>
+        <div>
+          {error && <div className="p-2 text-white border-gray-200 border-[1px] rounded-sm bg-red-400" style={{ color: 'red' }}>{error}</div>}
+        </div>
+      </form>
 
+    </div>
   );
 }
